9713d4231123ca5a974a57e77992f5ce70fd5aa4
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2024-12-02 12:05:02 +0530

    initialize project

---
+++ components/react-easy-crop/rotate.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { FC } from 'react';
+import Cropper from 'react-easy-crop';
+
+const Rotate = () => {
+    return (
+        <div>
+            <Cropper />
+        </div>
+    );    
+}
+
+export default Rotate;
