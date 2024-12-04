a757e030b34c23feeb2add4a37124807cd17bbb6
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2024-12-04 18:56:28 +0530

    iterate images

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,13 @@ import styles from "./page.module.css";
 import Carousel from "@/components/Carousel/Carousel";
 
 export default function Home() {
+  const imageArr = [1, 2, 3, 4];
   return (
     <div className={styles.page}>
       <Carousel>
-        <div>
-          one
-        </div>
-        <div>
-          two
-        </div>
-        <div>
-          three
-        </div>
+        {imageArr.map((img: number) => (
+          <Image width={100} height={200} src={`/images/${img}.avif`} alt={`${img}`} />
+        ))}
       </Carousel>
     </div>
   );
